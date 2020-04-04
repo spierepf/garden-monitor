8a9cdb98e1738a949fdd59f2b31f3068913a8310
Peter-Frank Spierenburg <spierepf@hotmail.com>
2020-04-04 16:23:52 +0000

    Added basic mosquitto example

--- pyboard/app.py
+++ pyboard/app.py
@@ -1,1 +1,10 @@
+from umqtt.simple import MQTTClient
+
+import etc.mosquitto
+
+c = MQTTClient("garden-monitor", etc.mosquitto.host)
+c.connect()
+c.publish(b"foo_topic", b"hello")
+c.disconnect()
+
 print("Hello World!")
